9e091864386a0546c0b854a591051dc021283fb6
Joao Gonçalo Santos <fc57103@alunos.fc.ul.pt>
2023-12-08 18:31:01 +0000

    AST working as expected.

--- my_ast.py
+++ my_ast.py
@@ -1,4 +1,5 @@
 import ast
+import astexport.export
 import json
 
 def extract_ast(filename):
@@ -6,17 +7,9 @@ def extract_ast(filename):
         py_str = fp.read()
 
     ast_py = ast.parse(py_str)
-    return ast_to_dict(ast_py)
-
-def ast_to_dict(node):
-    if isinstance(node, ast.AST):
-        node_dict = {'ast_type': type(node).__name__, 'lineno': getattr(node, 'lineno', None)}
-        node_dict.update({field: ast_to_dict(getattr(node, field)) for field in node._fields})
-        return node_dict
-    elif isinstance(node, list):
-        return [ast_to_dict(item) for item in node]
-    else:
-        return node
+    ast_dict = astexport.export.export_dict(ast_py)
+    
+    return ast_dict
 
 # Example usage:
 filename = "hello_world.py"
