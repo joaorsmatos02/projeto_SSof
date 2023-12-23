14fee57f4292a9433792af8dd176cb9ce2d993b9
Joao Gonçalo Santos <fc57103@alunos.fc.ul.pt>
2023-12-23 17:16:09 +0000

    Main dev

--- AST_parser.py
+++ AST_parser.py
@@ -1,11 +1,8 @@
 import ast
 import astexport.export
 
-def extract_ast(filename):
-    with open(filename, "r") as fp:
-        py_str = fp.read()
-
-    ast_py = ast.parse(py_str)
+def extract_ast(slice):
+    ast_py = ast.parse(slice)
     ast_dict = astexport.export.export_dict(ast_py)
     
     return ast_dict
--- Main.py
+++ Main.py
@@ -1,25 +1,22 @@
 import sys
 import json
-
 from Pattern import Pattern
 from AST_parser import extract_ast
 
-
 if __name__ == "__main__":
-    #if len(sys.argv) < 2:
-    #    print("usage: python main.py <slice-path> <pattern-path>")
-    #    exit()
+    with open(sys.argv[1], "r") as f:
+        slice_content = f.read()
 
-    #slicePath = "slices\\basic-flow.py"#sys.argv[1]
-    slicePath = "slices\\Specification\\slices\\1a-basic-flow.patterns.json"#sys.argv[1]
+    with open(sys.argv[2], "r") as fp:
+        patterns = json.load(fp)
 
-    patternPath = "slices\\basic-flow.patterns.json"#sys.argv[2]
+    # Print patterns
+    print(json.dumps(patterns, indent=4))
 
-    ast = extract_ast(slicePath)
-    
-    with open(patternPath, 'r') as f:
-        pattern = f.read()
-
-    for p in json.loads(pattern):
+    for p in patterns:
         pattern = Pattern(p)
-        print("aaa")
+        # Do something with the pattern if needed
+
+    ast_dict = extract_ast(slice_content)
+
+    print(ast_dict)
