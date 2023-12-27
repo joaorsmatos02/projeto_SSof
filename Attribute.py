2a771236507d759484a7b2ebc591ce35d4628778
Joao Gonçalo Santos <fc57103@alunos.fc.ul.pt>
2023-12-27 23:45:25 +0000

    Attribute changes, but not working.

--- Attribute.py
+++ Attribute.py
@@ -17,6 +17,18 @@ class Attribute:
     def eval(self, policy, multilabelling, vulnerabilities):
         print(repr(self))
         
+        eval = self.value.eval(policy, multilabelling, vulnerabilities)
+        
+        #check if the left part of the attributte is uninstatiated
+        all_patterns = policy.getAllPatterns()
+        if multilabelling.get_Multilabel(self.value.value) != None and multilabelling.get_Multilabel(self.value.value).get_labels() == {} :
+            for pattern in all_patterns:
+                new_label = Label()
+                new_label.add_source(self.value.value, self.line_number)
+                multilabelling.get_Multilabel(self.value.value).add_label(pattern.get_vulnerability(), new_label)
+            multilabelling.update_Multilabel(self.attribute,  multilabelling.get_Multilabel(self.value.value))
+        
+        
         patterns_where_is_source = policy.get_patterns_where_value_is_source(self.attribute)
         multiLabel = MultiLabel()
 
@@ -27,4 +39,4 @@ class Attribute:
 
         multilabelling.update_Multilabel(self.attribute, multiLabel)
         
-        return [self.value.eval(policy, multilabelling, vulnerabilities), self.attribute]
+        return self.attribute
